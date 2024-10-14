d99399dee1e8f367b26fa996c198fcc09252ee1a
Bharath <bharathj222000@gmail.com>
2024-10-14 12:27:00 +0530

    add email functionality & small updates

--- script.js
+++ script.js
@@ -1,60 +1,82 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass('sticky');
-        } else {
-            $('.navbar').removeClass('sticky');
-        }
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass('show');
-        } else {
-            $('.scroll-up-btn').removeClass('show');
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
 
-    // Slide Up Scripts
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 })
-    })
+  // Slide Up Scripts
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass('active');
-        $('.menu-btn i').toggleClass('active');
-    });
-    // typing animation script
-    var typed = new Typed(".typing", {
-        strings: ['Web Developer', 'Programmer', 'Designer'],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-2", {
-        strings: ['Developer', 'Programmer', 'Designer'],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+  // typing animation script
+  var typed = new Typed(".typing", {
+    strings: ["Web Developer", "Programmer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Programmer", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: false
-            },
-            1000: {
-                items: 3,
-                nav: false
-            }
-        }
-    })
-})
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
+
+document.getElementById("contactForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const templateParams = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs.send("service_l7p4xhp", "template_0o87dd8", templateParams).then(
+    (response) => {
+      alert("Your message was sent successfully!");
+      document.getElementById("contactForm").reset();
+    },
+    (error) => {
+      alert("There was an error sending your message. Please try again.");
+      console.log("Failed...", error);
+    }
+  );
+});
